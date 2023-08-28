ca3321398f80591121b9b02fc8ec05e791156bc8
jessica-pires <jessica_dutra19@outlook.com>
2023-08-28 18:07:50 -0300

    refazer

--- desafio02/main.js
+++ desafio02/main.js
@@ -2,18 +2,22 @@ function verificar(){
   var data = new Date() //data atual
   var ano = data.getFullYear()  //ano atual
   var anoUsuario = document.querySelector('#txtano')  //anodo usuario
-  var res = document.querySelector('#res') //texto 
+  var res = document.querySelector('#res') //texto
+  
   if (anoUsuario.value.length == 0 || Number(anoUsuario.value) > ano){
     window.alert('[ERRO] Verifique os dados e tente novamente')
   } else {
     var forsex = document.querySelector('#sexo')
     var idade = ano - Number(anoUsuario.value)
-    var genero =''
+    var genero =' '
     if (forsex[0].checked){
-      genero = 'homem'
+      genero = 'homem';
     } else if (forsex[1].checked){
-      genero = 'mulher'
+      genero = 'mulher';
     }
-    res.innerHTML = `detectamos ${genero} com ${idade} anos!`
-  }   
+  res.innerHTML = `Voce é ${gereno} com ${idade}`
+  
+  
+  }
+
 }
